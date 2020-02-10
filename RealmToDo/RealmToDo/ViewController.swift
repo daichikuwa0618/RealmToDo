28f082beb2330b4c3845f08236c7de221789c323
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-02-10 14:05:19 +0900

    [ADD] complete TableView cell

--- RealmToDo/RealmToDo/ViewController.swift
+++ RealmToDo/RealmToDo/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: @IBOutlet
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputTableView: UITableView!
+
+    // TodoModel 型変数，Realm から受け取った値を入れる変数
+    var itemList: Results<TodoModel>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let realm = try! Realm()
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+
+        // Realm データベースに格納されている値を全て取得する
+        self.itemList = realm.objects(TodoModel.self)
     }
 
+    @IBAction func tapButton(_ sender: Any) {
+
+        let todoModel: TodoModel = TodoModel()
+
+        todoModel.items = self.inputTextField.text
+
+        // get Realm data base
+        let realm = try! Realm()
+
+        // add records to data base
+        try! realm.write {
+            realm.add(todoModel)
+        }
+
+        // reload table view
+        self.outputTableView.reloadData()
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.itemList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+
+        let item: TodoModel = self.itemList[(indexPath as NSIndexPath).row]
+
+        cell.textLabel?.text = item.items
+
+        return cell
+    }
+
+}
